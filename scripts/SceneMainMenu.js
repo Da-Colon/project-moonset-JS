fc71557c5a1493a27ef892bdd99c0c8fe9f5afb6
Da-Colon <dcolonantonio@gmail.com>
2019-10-14 12:42:34 -0400

    added more preload images and deleted unused files

--- scripts/SceneMainMenu.js
+++ scripts/SceneMainMenu.js
@@ -2,6 +2,11 @@ class SceneMainMenu extends Phaser.Scene {
     constructor() {
         super({ key: "SceneMainMenu" });
     }
+    preload() {
+        this.load.image("play-button", "./assets/img/buttons/play-button.png");
+        this.load.image("quit-button", "./assets/img/buttons/quit-button.png");
+
+    }
     create() {
         this.scene.start("SceneMain");
     }
--- scripts/sceneMain.js
+++ scripts/sceneMain.js
@@ -6,12 +6,13 @@ class SceneMain extends Phaser.Scene {
         this.load.image("(space-background)", "./assets/img/bg-space.jpg");
         // PLAYER SPRITES
         this.load.image("player-one", "./assets/img/playerships/player-one-ship.png");
-        this.load.image("player-bullet", "./assets/img/bullets/player-bullet");
+        this.load.image("player-bullet", "./assets/img/bullets/player-bullet.png");
         // ENEMY SPRITES
         this.load.image("enemy-one", "./assets/img/enemyships/enemyship01.png");
         this.load.image("enemy-two", "./assets/img/enemyships/enemyship02.png");
         this.load.image("enemy-three", "./assets/img/enemyships/enemyship03.png");
-        this.load.image("enemy-bullet", "./assets/img/bullets/enemy-bullet");
+        this.load.image("enemy-bullet", "./assets/img/bullets/enemy-bullet.png");
+
         // EXPLOSIONS SPRITES
         this.load.image("explosion-00", "./assets/img/explosions/regularExplosion00.png");
         this.load.image("explosion-01", "./assets/img/explosions/regularExplosion01.png");
@@ -24,8 +25,8 @@ class SceneMain extends Phaser.Scene {
         this.load.image("explosion-08", "./assets/img/explosions/regularExplosion08.png");
 
         // AUDIO FILES
-        this.load.audio("enemy-explosion", "assets/snd/enemy-explosion.wav");
-        this.load.audio("player-explosion", "assets/snd/player-explosion.wav");
+        this.load.audio("explosion01", "assets/snd/enemy-explosion.wav");
+        this.load.audio("explosion02", "assets/snd/player-explosion.wav");
         this.load.audio("laser-sound", "assets/snd/Laser_Shoot.wav");
 
         // this.load.image("(insert-imageKey)", "./assets/img/(insert-image)")
@@ -34,6 +35,32 @@ class SceneMain extends Phaser.Scene {
     }
 
     create() {
+        // EXPLOSION ANIMATION
+        this.anims.create({
+            key: 'explosion',
+            frames: [
+                { key: 'explosion-00' },
+                { key: 'explosion-01' },
+                { key: 'explosion-02' },
+                { key: 'explosion-03' },
+                { key: 'explosion-04' },
+                { key: 'explosion-05' },
+                { key: 'explosion-06' },
+                { key: 'explosion-07' },
+                { key: 'explosion-08' }
+            ],
+            frameRate: 10,
+            repeat: 1
+        });
+
+        // EXPLOSION SOUNDS
+        this.sfx = {
+            explosions: [
+                this.sound.add("explosion01"),
+                this.sound.add("explosion02")
+            ],
+            laser: this.sound.add("laser-sound")
+        }
 
     }
 }
